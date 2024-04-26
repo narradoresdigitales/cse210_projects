a699d02f889c2bf023c6797403c002929f3e80b4
sil <narradoresdigitales@gmail.com>
2024-04-26 05:23:19 -0500

    updates to prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,25 +1,48 @@
-using System.Collections.Generic;
-
+using System;
+using System.Collections.Generic; 
 
 class Program
 {
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-		int userEntry = -1;
+		int userNum = -1;
 
-		while (userEntry != 0)
+		while (userNum != 0)
 
 		{
-			Console.Write("Think positive and enter a list of positive numbers!");
-			Console.Write("Enter the number 0 when you have finished.");
-
-
+			Console.Write("Make a list of positive numbers (enter 0 to quit: ");
+			string userEntry = (Console.ReadLine());
+			userNum = int.Parse(userEntry);
+		
+		}
+		if (userNum != 0)
+		{
+			numbers.Add(userNum);
 		}
 
+		int sum = 0;
+		foreach (int number in numbers)
+		{
+			sum += number;
+		}
 
+		Console.WriteLine($"The sum of the list is: {sum}.");
 
+		float average = ((float)sum) / numbers.Count;
+		Console.WriteLine($"The average for the list is {average}.");
 
+		int max = numbers[0];
+
+		foreach (int number in numbers)
+		{
+			if (number > max)
+			{
+				max = number;
+			}
+		}
+
+		Console.WriteLine($"The max number for the list is {max}.");
     
     }
 }
