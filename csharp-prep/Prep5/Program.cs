97107a701e029787f407be3a02beeb51e523a9f0
sil <narradoresdigitales@gmail.com>
2024-04-27 06:45:43 -0500

    updated prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,15 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        DisplayMessage();
-		Username();
+        DisplayWelcome();
+		string userName = PromptUsername();
+		int userNum = PromptUserNumber();
+		int squaredNum = SquareNumber(userNum);
+		DisplayResult(userName, squaredNum);
 		
-		static void DisplayMessage()
+		static void DisplayWelcome()
 		{
     		Console.WriteLine("Welcome to the Program!");
 		}
 		
-		static string Username()
+		static string PromptUsername()
 			
 		{
 			Console.Write("Please enter your name:   ");
@@ -20,5 +23,26 @@ class Program
 			
 			return username;
 		}
+
+		static int PromptUserNumber()
+		{
+			Console.Write("Enter your favorite number: ");
+			string response = Console.ReadLine();
+			int userNum = int.Parse(response);
+
+			return userNum;
+		}
+
+		static int SquareNumber(int number)
+		{
+			int square = number * number;
+			return square;
+		}
+
+		static void DisplayResult(string name, int square)
+		{
+			Console.WriteLine($"{name}, the square of your number is {square}.");
+		}
+
     }
 }
