bec7caed3933d94e6ca6ae9c03cb2226b1afd1a4
sil <narradoresdigitales@gmail.com>
2024-05-03 05:17:49 -0500

    working on prep2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
+using System.Collections.Generic;
+using System.Net;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
+        int userChoice = -1;
+
+        while (userChoice != 5)
+
+        {
+        Console.Write("Please select one of the following choices \n 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5. Quit \n ");
+        Console.Write("What would you like to do? ");
+        string userEntry = Console.ReadLine();
+        userChoice = int.Parse(userEntry);
+        
+        }
+    
+            if (userChoice == 1)
+            {
+            Console.WriteLine("You have chosen to write a journal entry");
+            }
+
+
+        
     }
+
+    
 }
