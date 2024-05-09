883add26b4f62df9bc0cb94488918d233bb5b90d
sil <narradoresdigitales@gmail.com>
2024-05-09 11:02:54 -0500

    updates to dev3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,6 +8,8 @@ class Program
     
         
         Reference r1 = new Reference("Proverbs", 3, 5,6);
+        Console.WriteLine();
+        Console.WriteLine();
         Console.Write(r1.GetDisplayText());
         
         Scripture scripture1 = new Scripture(r1, "text");
@@ -15,19 +17,31 @@ class Program
 
         Console.WriteLine();
         Console.WriteLine();
-        Console.Write("Press enter to continue or type 'quit' to finish: \n ");
+        Console.Write("Press enter to continue or type 'quit' to finish: \n  > ");
         var line = Console.ReadLine();
 
         //I referenced the 'stack overflow' discussion to write 'if' syntax to read a null line or when the user presses 'enter.'
         //https://stackoverflow.com/questions/32655870/how-to-read-enter-from-the-keyboard-to-exit-program
 
         if (string.IsNullOrEmpty(line)) 
+        {
             Console.WriteLine("Let's begin to study the scriptures.");
+            Console.WriteLine();
+            Console.WriteLine(  scripture1.GetDisplayText());
+            
+        }
+                    
         else
         {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The program has been terminated.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
             Environment.Exit(0);
         }
 
-
+        
     } 
 }  
