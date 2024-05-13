76cf140ded393cd086a1343fadd98be85dca42a5
sil <narradoresdigitales@gmail.com>
2024-05-13 12:53:55 -0500

    edits learn4

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -3,18 +3,26 @@ public class Assignment
     private string _studentName;
     private string _topic; 
 
-    public Assignment (string name, string topic)
+    public Assignment(string student, string topic)
     {
-        _studentName = name;
+        _studentName = student;
         _topic = topic;
     }
+    
+    public string GetStudent()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }s
 
     public string GetSummary()
     {
-        return ($"{_studentName} - {_topic}");
+        return $"{_studentName} - {_topic}";
     }
-
-
 
     
 
--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -1,5 +1,27 @@
-public class MathAssignment
+public class MathAssignment : Assignment
 
 {
-    
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string student, string topic, string textbookSection, string problems) : base(student, topic)
+    {
+        _textbookSection = section;
+    }
+
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+    public void SetProblems(string problems)
+    {
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+
+
 }
