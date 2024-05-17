f7f8861395e22e865785225a0b55e8e5db9784d8
sil <narradoresdigitales@gmail.com>
2024-05-17 09:11:52 -0500

    added Random methods to display prompts and questions

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -53,14 +53,34 @@ public class ReflectingActivity : Activity
 
     public void DisplayPrompt() 
     {
+        _prompts.Add("How did you feel when it was complete?");
+        _prompts.Add("Think of a time when you stood up for someone else.");
         _prompts.Add("Think of a time when you did something really difficult.");
-        Console.WriteLine(_prompts[0]);
+        _prompts.Add( "Think of a time when you helped someone in need.");
+        _prompts.Add("Think of a time when you did something truly selfless.");
+
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        Console.WriteLine(_prompts[index]);
+        
     }
 
     public void DisplayQuestions()
     {
+        _questions.Add("Think of a time when you did something really difficult.");
+        _questions.Add(" Why was this experience meaningful to you?");
+        _questions.Add(" Have you ever done anything like this before?");
+        _questions.Add("How did you get started?");
         _questions.Add("How did you feel when it was complete?");
-        Console.WriteLine(_questions[0]);
+        _questions.Add("What made this time different than other times when you were not as successful?");
+        _questions.Add("What is your favorite thing about this experience?");
+        _questions.Add(" What could you learn from this experience that applies to other situations?");
+        _questions.Add("What did you learn about yourself through this experience?");
+        _questions.Add("How can you keep this experience in mind in the future?");
+        
+        Random random = new Random();
+        int index = random.Next(_questions.Count);
+        Console.WriteLine(_questions[index]);
     }
 
 
