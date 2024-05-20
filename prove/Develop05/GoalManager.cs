7080639f7140548e290b08806532d87ca05e1c6f
sil <narradoresdigitales@gmail.com>
2024-05-20 12:30:13 -0500

    revisions to GoalManager.cs

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel;
+
 public class GoalManager
 
 {
@@ -9,7 +11,112 @@ public class GoalManager
 
     public void Start()
     {
+        Console.WriteLine("_________________________________________________"); //spacing
+        Console.WriteLine(); //spacing
         DisplayPlayerInfo();
+
+
+
+        Console.WriteLine("_________________________________________________"); //spacing
+        Console.WriteLine(); //spacing
+        Console.WriteLine(); //spacing
+        var choice = "";
+        do{
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(); //spacing
+            Console.WriteLine(" 1. Create New Goal");
+            Console.WriteLine(" 2. List Goals");
+            Console.WriteLine(" 3. Save Goals");
+            Console.WriteLine(" 4. Load Goals");
+            Console.WriteLine(" 5. Record Event");
+            Console.WriteLine(" 6. Quit:");
+
+            Console.WriteLine("_________________________________________________"); //spacing
+            Console.WriteLine(); //spacing
+            Console.WriteLine(); //spacing
+            Console.Write(" Select a choice from the menu: > ");
+            choice = Console.ReadLine();
+            
+            if (choice == "1")
+            {
+                //menu for Creating a New Goal
+                var choice1 = "";
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine(); //spacing
+                Console.WriteLine(" 1. Simple Goal");
+                Console.WriteLine(" 2. Eternal Goals");
+                Console.WriteLine(" 3. Checklist Goals");
+                Console.WriteLine(); //spacing
+                Console.Write(" Which type of goal would you like to create?  > ");
+                choice = Console.ReadLine();
+                
+
+                if (choice1 == "1")
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("You have chosen to make a Simple Goal.");
+                    Console.WriteLine();
+                    string name = Console.ReadLine();
+                    Console.WriteLine("What is a short description of it? ");
+                    string description = Console.ReadLine();
+                    Console.WriteLine("What is its point value? ");
+                    string points = Console.ReadLine();
+                    SimpleGoal simpleGoal = new SimpleGoal(name, description, points);
+                    _goals.Add(simpleGoal);
+
+                }
+                else if (choice1 == "2")
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("You have chosen to make an Eternal Goal.");
+                    Console.WriteLine();
+                }
+                else if (choice1 == "3")
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("You have chosen to make an Checklist Goal.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("Invalid Choice. Try again.");
+                    Console.WriteLine();
+                }
+                                
+            }
+            else if (choice == "2")
+            {
+                
+                
+            }
+            else if (choice == "3")
+            {
+                
+                
+            }
+            else if (choice == "4")
+            {
+                
+                
+            }
+            else if (choice == "5")
+            {
+                
+                
+            }
+            else if (choice == "6")
+            {
+                Console.WriteLine("You selected to exit. Goodbye");
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice. Try again.");
+            }
+
+        }
+        while (choice != "6");
     }
 
     public void DisplayPlayerInfo()
@@ -24,6 +131,8 @@ public class GoalManager
 
     public void CreateGoal()
     {
+        Console.WriteLine("What is the name of your goal?");
+
         
     }
 
