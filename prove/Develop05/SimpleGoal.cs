115a8ece469f533d4575716be3567b575885c4b6
sil <narradoresdigitales@gmail.com>
2024-05-20 11:35:36 -0500

    edits to SimpleGoal.cs, SimpleGoal constructor

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -6,7 +6,9 @@ public class SimpleGoal : Goal
 
     public SimpleGoal(string name, string description, string points) : base(name, description, points)
     {
-
+        _shortName = name;
+        _description = description;
+        _points = points;
     }
 
 
